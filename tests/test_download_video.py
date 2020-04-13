b918c2ff1ebb9a3dd9d4efa700bb759cd45abc4e
weimingdan <1508281800@qq.com>
2020-04-13 16:48:00 +0800

    commit something

--- tests/test_download_video.py
+++ tests/test_download_video.py
@@ -1,4 +1,11 @@
+
+
 import pytest
+from faker import Faker
+import arrow
+from tenacity import retry, stop_after_attempt
+import random
+
 
 
 class TestBaiduSpider(object):
@@ -6,4 +13,36 @@ class TestBaiduSpider(object):
         assert 1 + 1 == 2
 
     def test_false(self):
-        assert False
+        fak = Faker('zh_CN')
+        print('name: ', fak.name())
+        print('address: ', fak.address())
+        print('text: ', fak.text())
+        assert True
+
+    def test_parse_time(self):
+        time_str = '2020-1-4'
+        cc = arrow.get(time_str)
+        print(cc)
+        local = arrow.utcnow()
+        uu = local.shift(hour=1)
+        print(uu)
+        print(local.format())
+        print(local.humanize(locale='zh_CN'))
+        assert True
+
+    @retry(stop=stop_after_attempt(7))
+    def do_something(self):
+        print("I'm retrying!!!")
+        raise Exception
+
+    def test_retry(self):
+        print(self.get_info())
+        assert True
+
+    @retry
+    def get_info(self):
+        if random.randint(0, 10) > 1:
+            print('random number error')
+            raise IOError("Error occurred!!")
+        else:
+            return "HOHO"
